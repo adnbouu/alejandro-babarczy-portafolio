981030ec8650ec0980a25fab297eb9e7ecbf348e
adnbou <adnbou2009@gmail.com>
2024-02-27 00:12:00 -0500

    Fix Bugs

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 var icon = document.getElementById("icon");
+var moon = document.getElementsByClassName(".bxs-moon");
 
 icon.onclick = function () {
   document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    icon.innerHTML = "<i class='bx bxs-sun'></i>";
+  } else {
+    icon.innerHTML = "<i class='bx bxs-moon'></i>";
+  }
 };
